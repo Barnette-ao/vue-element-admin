9689039d6bcc223289733bf251e80028201ac792
Barnette-ao <58058378+Barnette-ao@users.noreply.github.com>
2022-12-05 15:11:26 +0800

    解决登录跨域问题，登录接口跳转到httop:127.0.0.0:3000

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,7 @@ const service = axios.create({
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
-
+console.log('process.env', process.env)
 // request interceptor
 service.interceptors.request.use(
   config => {
